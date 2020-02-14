8ea0f601832021c01cfe46820a13bc911c67d998
NataliaB <nataliyya24@gmail.com>
2020-02-14 17:17:03 +0200

    Учебный проект: нас орда

---
+++ js/setup.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+
+var getRandomArray = function (adArray) {
+  var randomArray = Math.floor(Math.random() * adArray.length);
+  return randomArray;
+};
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+
+
+var wizards = [];
+var getWizard = function () {
+
+  for (var i = 0; i < 4; i++) {
+    var wizard = {
+      name: NAMES[getRandomArray(NAMES)] + ' ' + SURNAMES[getRandomArray(SURNAMES)],
+      coatColor: COATS[getRandomArray(COATS)],
+      eyesColor: EYES[getRandomArray(EYES)]
+    };
+
+    wizards.push(wizard);
+  }
+
+  return wizards;
+};
+getWizard();
+
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
